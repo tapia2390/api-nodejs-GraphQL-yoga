9179237ae325aed2fb241b8fde98e22de90744ef
yhonatantapias <yhonatantapias@destiny.ws>
2024-02-01 17:04:27 -0500

    api node js con grapql

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql'); 
+var { buildSchema } = require('graphql');
+
+
+// Construimos el schema
+var schema = buildSchema(`
+
+  type Truck  {
+	id: Int
+	brand: String
+    model: String
+    color: String
+    numberId: String
+    isAvailable: Boolean
+	}
+
+  type Query {
+    	trucks: [Truck]
+    	Truck(id: Int): Truck
+  	}
+
+  type Mutation {
+    	addTruck(brand: String, model: String, color: String, numberId: String, isAvailable: Boolean): Truck
+      	}
+
+`);
+
+var trucks = [];
+var counter=1;
+
+// Función para resolver las peticiones
+var root = {
+    trucks: () => { return trucks; },
+
+    truck: ( data ) => { 
+	for ( var i=0; i<trucks.length; i++ ) 
+		if ( trucks[i].id==data.id ) 
+			return trucks[i]; 
+
+	return null; 
+	},
+
+  addTruck: ( data ) => { 
+	var c={ 'id': counter, 'brand':data.brand, 'model':data.model, 'color':data.color, 'numberId':data.numberId , 'isAvailable':data.isAvailable }; 
+	trucks.push( c ); 
+	counter++; 
+	return c; 
+	},
+};
+
+// Arrancamos el servidor web
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('GraphQL API en http://localhost:4000/graphql');
